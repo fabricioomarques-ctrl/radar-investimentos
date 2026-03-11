89f99b55b0b5649db59fdd0b7e2228d4bcf1bc57
fabricioomarques-ctrl <fabricioo.marques@gmail.com>
2026-03-11 17:31:00 -0300

    Update calc.py

--- utils/calc.py
+++ utils/calc.py
@@ -1,1 +1,15 @@
+def gross_return(rate_cdi, cdi):
+    return (rate_cdi / 100) * cdi
 
+
+def net_return(gross, days):
+    if days <= 180:
+        ir = 0.225
+    elif days <= 360:
+        ir = 0.20
+    elif days <= 720:
+        ir = 0.175
+    else:
+        ir = 0.15
+
+    return gross * (1 - ir)
