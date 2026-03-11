dbca8aeef5c3b45b9c3e128567255b79983776be
fabricioomarques-ctrl <fabricioo.marques@gmail.com>
2026-03-11 17:32:13 -0300

    Update fallback.py

--- collectors/fallback.py
+++ collectors/fallback.py
@@ -1,1 +1,24 @@
-
+def get_fallback():
+    return [
+        {
+            "bank": "Simulação Interna",
+            "type": "CDB",
+            "rate": 110,
+            "days": 365,
+            "liquidity": False
+        },
+        {
+            "bank": "Simulação Interna",
+            "type": "CDB",
+            "rate": 102,
+            "days": 1,
+            "liquidity": True
+        },
+        {
+            "bank": "Simulação Interna",
+            "type": "LCI",
+            "rate": 92,
+            "days": 365,
+            "liquidity": False
+        }
+    ]
